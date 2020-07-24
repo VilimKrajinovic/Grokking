0b38c457497dd9deed12bed8e1a840437df12b09
Vilim <vilim.krajinovic@gmail.com>
2020-07-24 18:35:31 +0200

    LongestSubstringWithKDistinctCharacters: Added a no streams faster version

--- src/SlidingWindow/LongestSubstringWithKDistinctCharacters.java
+++ src/SlidingWindow/LongestSubstringWithKDistinctCharacters.java
@@ -1,7 +1,10 @@
 package SlidingWindow;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class LongestSubstringWithKDistinctCharacters {
 
@@ -18,8 +21,30 @@ public class LongestSubstringWithKDistinctCharacters {
 
             max = Math.max(max, builder.toString().length());
         }
-
         return max;
+    }
+
+    public static int findLengthNoStreams(String str, int k) {
+        int windowStart = 0;
+        int maxLength = 0;
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightCharacter = str.charAt(windowEnd);
+            frequencyMap.put(rightCharacter, frequencyMap.getOrDefault(rightCharacter, 0) + 1);
+
+            while (frequencyMap.size() > k) {
+                char leftCharacter = str.charAt(windowStart);
+                frequencyMap.put(leftCharacter, frequencyMap.get(leftCharacter) - 1);
+                if (frequencyMap.get(leftCharacter) == 0) {
+                    frequencyMap.remove(leftCharacter);
+                }
+                windowStart++;
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+
+        return maxLength;
     }
 
     public static int distinctCharacters(String str) {
@@ -34,5 +59,11 @@ public class LongestSubstringWithKDistinctCharacters {
         System.out.println("Length of the longest substring: " + findLength("cbbebi", 3));
         System.out.println("Length of the longest substring: " + findLength("eceba", 2));
         System.out.println("Length of the longest substring: " + findLength("a", 1));
+
+        System.out.println("Length of the longest substring: " + findLengthNoStreams("araaci", 2));
+        System.out.println("Length of the longest substring: " + findLengthNoStreams("araaci", 1));
+        System.out.println("Length of the longest substring: " + findLengthNoStreams("cbbebi", 3));
+        System.out.println("Length of the longest substring: " + findLengthNoStreams("eceba", 2));
+        System.out.println("Length of the longest substring: " + findLengthNoStreams("a", 1));
     }
 }
