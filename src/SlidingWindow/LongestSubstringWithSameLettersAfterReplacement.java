c4f2106f53084300e360fa28c45963e49fb2c659
vkrajinovic <vilim.krajinovic@infobip.com>
2020-07-26 13:11:01 +0200

    SlidingWindow: Added LongestSubstringWithSmaeLettersAfterReplacmenet task

---
+++ src/SlidingWindow/LongestSubstringWithSameLettersAfterReplacement.java
@@ -0,0 +1,37 @@
+package slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithSameLettersAfterReplacement {
+
+    public static int findLength(String str, int k) {
+        int windowStart = 0;
+        int maxLength = 0;
+        int maxRepeatLetterCount = 0;
+
+        Map<Character, Integer> letterFrequencyMap = new HashMap<>();
+
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            letterFrequencyMap.put(rightChar, letterFrequencyMap.getOrDefault(rightChar, 0) + 1);
+            maxRepeatLetterCount = Math.max(maxRepeatLetterCount, letterFrequencyMap.get(rightChar));
+
+            if (windowEnd - windowStart + 1 - maxRepeatLetterCount > k) {
+                char leftChar = str.charAt(windowStart);
+                letterFrequencyMap.put(leftChar, letterFrequencyMap.get(leftChar) - 1);
+                windowStart++;
+            }
+
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findLength("aabccbb", 2));
+        System.out.println(findLength("abbcb", 1));
+        System.out.println(findLength("abccde", 1));
+    }
+}
